4b4ae56510ecf970970d1a8240a2fdf6825f3b74
redfoxbg <redfoxbg@gmail.com>
2023-09-08 01:27:34 +0300

    Adding Assets and setup background

---
+++ Firework.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spawn_Animated_Fireworks_WFApp
+{
+    internal class Firework
+    {
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -2,14 +2,35 @@ namespace Spawn_Animated_Fireworks_WFApp
 {
     public partial class Form1 : Form
     {
+        private readonly string assetsPath = Path.Combine(Application.StartupPath)
+           .Replace("bin\\Debug\\net6.0-windows\\", "Assets\\");
+
+        List<string> imageLocation = new List<string>();
+        List<Firework> fireworksList = new List<Firework>();
+        int backgroundNumber;
+
         public Form1()
         {
             InitializeComponent();
+            SetUp();
+        }
+
+        private void SetUp()
+        {
+            imageLocation = Directory.GetFiles(assetsPath + "backgrounds", "*.jpg").ToList();
+            this.BackgroundImage = Image.FromFile(imageLocation[0]);
+            this.BackgroundImageLayout = ImageLayout.Stretch;
         }
 
         private void KeyIsUp(object sender, KeyEventArgs e)
         {
+            if (e.KeyCode == Keys.Space)
+            {
+                if (backgroundNumber < imageLocation.Count - 1) backgroundNumber++;
+                else backgroundNumber = 0;
 
+                this.BackgroundImage = Image.FromFile(imageLocation[backgroundNumber]);
+            }            
         }
 
         private void FormMouseDown(object sender, MouseEventArgs e)
