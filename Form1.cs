d9641619df034a276978b3d194a068c30c2af908
redfoxbg <redfoxbg@gmail.com>
2023-09-08 00:52:31 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+namespace Spawn_Animated_Fireworks_WFApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void FormMouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void FormPaintEvent(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void AnimationTimerEvent(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
